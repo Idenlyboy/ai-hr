d7019856f364906285af9f064a5efa6785bbf3ad
kazantsev <kazantsev@stdkit.ru>
2025-09-01 16:16:38 +0300

    Create raw views. Create middlewares for routes and configs

---
+++ resources/views/auth/registration.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app.guest')
+
+@section('title', 'Регистрация')
+
+@section('content')
+
+    <reg-form></reg-form>
+
+@endsection
---
+++ resources/views/layouts/app/user.blade.php
@@ -0,0 +1,19 @@
+@include('layouts.head')
+
+<body>
+    <main id="app">
+
+        @section('header')
+        @include('layouts.header.layout')
+        @show
+
+        @yield('content')
+
+        @section('footer')
+        @include('layouts.footer.layout')
+        @show
+    </main>
+    <script src="{{ mix('js/app.js') }}" async></script>
+</body>
+
+</html>
